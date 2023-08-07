a333700c1ea3b77df738418dffad776e6a6a3df9
pierreamir123 <pierre.farag00@eng-st.cu.edu.eg>
2023-08-07 16:07:45 +0300

    SlackServer Implementation

---
+++ Classes/SlackClass.py
@@ -0,0 +1,50 @@
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from slack_bolt.adapter.flask import SlackRequestHandler
+from slack_bolt import App
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, request
+
+class SlackClass :
+    def __init__(self):
+        # Load environment variables from .env file
+        load_dotenv(find_dotenv())
+        # Set Slack API credentials
+        self.SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+        self.SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
+        self.SLACK_BOT_USER_ID = os.environ["SLACK_BOT_USER_ID"]
+        # Initialize the Slack app
+        self.app = App(token=self.SLACK_BOT_TOKEN)
+        self.handler = SlackRequestHandler(self.app)
+        
+    
+
+    def GetBotUserId(self):
+        """
+        Get the bot user ID using the Slack API.
+        Returns:
+            str: The bot user ID.
+        """
+        try:
+            # Initialize the Slack client with your bot token
+            slack_client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+            response = slack_client.auth_test()
+            return response["user_id"]
+        except SlackApiError as e:
+            print(f"Error: {e}")
+
+
+    def MyFunction(self,text):
+        """
+        Custom function to process the text and return a response.
+        In this example, the function converts the input text to uppercase.
+
+        Args:
+            text (str): The input text to process.
+
+        Returns:
+            str: The processed text.
+        """
+        response = text.upper()
+        return response
--- SlackServer.py
+++ SlackServer.py
@@ -1,1 +1,36 @@
-import numpy as np
+from typing import Union
+from fastapi import FastAPI
+from Classes.SlackClass import SlackClass
+
+Server = FastAPI()
+
+SlackApp = SlackClass()
+
+@SlackApp.app.event("app_mention")
+def HandleMentions(body, say):
+    """
+    Event listener for mentions in Slack.
+    When the bot is mentioned, this function processes the text and sends a response.
+
+    Args:
+        body (dict): The event data received from Slack.
+        say (callable): A function for sending a response to the channel.
+    """
+    text = body["event"]["text"]
+
+    mention = f"<@{SLACK_BOT_USER_ID}>"
+    text = text.replace(mention, "").strip()
+
+    response = SlackApp.MyFunction(text)
+    say(response)
+
+@Server.post("/slack/events")
+async def SlackEvents():
+    """
+    Route for handling Slack events.
+    This function passes the incoming HTTP request to the SlackRequestHandler for processing.
+
+    Returns:
+        Response: The result of handling the request.
+    """
+    return SlackApp.handler.handle(request)
